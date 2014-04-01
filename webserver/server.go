63fe85d67395844bdaa7f0baf30792f023b19876
Andre Fonseca <aoqfonseca@gmail.com>
2014-04-01 18:59:32 -0300

    Fixing format: running gofmt in file server.go

--- webserver/server.go
+++ webserver/server.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "runtime"
-    "github.com/gorilla/pat"
-    "github.com/mediaRepository/api"
+	"github.com/gorilla/pat"
+	"github.com/mediaRepository/api"
+	"log"
+	"net/http"
+	"runtime"
 )
 
 func main() {
 
-    r := pat.New()
-    
-    //Creating routes
-    r.Get("/healthcheck", http.HandlerFunc(api.HealthCheckHandler))
-    r.Get("/upload", http.HandlerFunc(api.PostFormUploadFile))
-    r.Post("/upload", http.HandlerFunc(api.UploadFileHandler))
-    
-    log.Println("Staring server: getting num cpu")
-    runtime.GOMAXPROCS(runtime.NumCPU())
-    
-    log.Println("Server listening on 4321")
-    http.ListenAndServe(":4321", r)
+	r := pat.New()
+
+	//Creating routes
+	r.Get("/healthcheck", http.HandlerFunc(api.HealthCheckHandler))
+	r.Get("/upload", http.HandlerFunc(api.PostFormUploadFile))
+	r.Post("/upload", http.HandlerFunc(api.UploadFileHandler))
+
+	log.Println("Staring server: getting num cpu")
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	log.Println("Server listening on 4321")
+	http.ListenAndServe(":4321", r)
 }
-
-
