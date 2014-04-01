184ce8a08b83369b0a7e94f9aa91c34cad34d27a
Andre Fonseca <aoqfonseca@gmail.com>
2014-03-31 23:14:49 -0300

    Tests running

--- api/handler_test.go
+++ api/handler_test.go
@@ -1,12 +1,20 @@
 package api
 
 import (
-    "net/http",
-    "net/http/httptest",
-    "testing",
-    "strings",
-    "gocheck"
+    "testing"
+    "net/http"
+    "net/http/httptest"
+    "launchpad.net/gocheck"
+    "io"
+    "io/ioutil"
+    "strings"
 )
+
+func Test(t *testing.T) { gocheck.TestingT(t) }
+
+type S struct{}
+
+var _ = gocheck.Suite(&S{}) 
 
 func request(method, url string, b io.Reader, c *gocheck.C) (*httptest.ResponseRecorder, *http.Request) {
 	request, err := http.NewRequest(method, url, b)
@@ -23,11 +31,10 @@ func get(url string, b io.Reader, c *gocheck.C) (*httptest.ResponseRecorder, *ht
 
 
 func (s *S) TestHealthCheck(c *gocheck.C){
-    b := io.NewReader()
-    recorder, request := get('/healthcheck', b, c)
+    b := strings.NewReader("")
+    recorder, request := get("/healthcheck", b, c)
     HealthCheckHandler(recorder, request)
-    boy, err := io.ReadAll(recorder.Body)
-    c.Assert(err, gocheck.isNill)
-    c.Assert(body, gocheck.Equals,"WORKING")
+    body, err := ioutil.ReadAll(recorder.Body)
+    c.Assert(err, gocheck.IsNil)
+    c.Assert(string(body), gocheck.Equals,"WORKING")
 }
-
