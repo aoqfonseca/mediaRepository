a1bbddabfcbe483ee0241ca4bdf06d710f2fb542
Andre Fonseca <aoqfonseca@gmail.com>
2014-04-01 14:24:03 -0300

    Testing success upload file

--- api/handler_test.go
+++ api/handler_test.go
@@ -1,11 +1,15 @@
 package api
 
 import (
+    "bytes"
 	"io"
 	"io/ioutil"
 	"launchpad.net/gocheck"
+    "mime/multipart"
 	"net/http"
 	"net/http/httptest"
+    "os"
+    "path/filepath"
 	"strings"
 	"testing"
 )
@@ -28,6 +32,39 @@ func get(url string, b io.Reader, c *gocheck.C) (*httptest.ResponseRecorder, *ht
 	return request("GET", url, b, c)
 }
 
+func newRequestFormFile(url string, params map[string]string, paramName, path string)(*httptest.ResponseRecorder, *http.Request, error){
+    file, err := os.Open(path)
+    if err != nil{
+        return nil, nil, err
+    }
+
+    defer file.Close()
+
+    body := &bytes.Buffer{}
+    writer := multipart.NewWriter(body)
+    part, err := writer.CreateFormFile(paramName, filepath.Base(path))
+
+    if err != nil{
+        return nil, nil, err
+    }
+
+    _, err = io.Copy(part, file)
+
+    for key, val :=range params {
+        _ = writer.WriteField(key, val)
+    }
+    err = writer.Close()
+
+    if err != nil {
+        return nil, nil, err
+    }
+
+	recorder := httptest.NewRecorder()
+    request, err  := http.NewRequest("POST", url, body)
+    request.Header.Set("Content-Type", writer.FormDataContentType())
+    return recorder, request, err
+}
+
 func (s *S) TestHealthCheck(c *gocheck.C) {
 	b := strings.NewReader("")
 	recorder, request := get("/healthcheck", b, c)
@@ -35,4 +72,14 @@ func (s *S) TestHealthCheck(c *gocheck.C) {
 	body, err := ioutil.ReadAll(recorder.Body)
 	c.Assert(err, gocheck.IsNil)
 	c.Assert(string(body), gocheck.Equals, "WORKING")
+}
+
+func (s *S) TestUploadFile(c *gocheck.C) {
+    recorder, request, err := newRequestFormFile("/upload", nil, "file", "../fixtures/teste.jpg")
+    c.Assert(err, gocheck.IsNil)
+    UploadFileHandler(recorder, request)
+	body, err := ioutil.ReadAll(recorder.Body)
+
+    c.Assert(err, gocheck.IsNil)
+	c.Assert(string(body), gocheck.Equals, "SUCCESS")
 }
